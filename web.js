381ae4ee0e3fc30e0d1141302adef4510123c9ff
nitsshukla <nitincontactme@gmail.com>
2013-07-20 07:41:31 +0000

    ASDdd

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var fs  = require('fs');
 app.use(express.logger());
 var buffer = new Buffer("I'm a string!", "utf-8");
 app.get('/', function(request, response) {
-  response.send(buffer);
+
+response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
