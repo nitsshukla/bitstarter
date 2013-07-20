2d15be89d9ad7ec436b4844a4f0287d2fbe000e9
nitsshukla <nitincontactme@gmail.com>
2013-07-20 07:30:12 +0000

    ASDA

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var fs  = require('fs');
 app.use(express.logger());
-
+var buffer = new Buffer("I'm a string!", "utf-8");
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
