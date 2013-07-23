44eda7f97b8c6b19dc9c464b49cd145ed12f3555
nitsshukla <nitincontactme@gmail.com>
2013-07-23 19:48:22 +0000

    webb

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var app = express.createserver(express.logger());
-var fs  = require('fs');
+var fs=require('fs');
 var htmlfile="index.html";
+
+var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
 var html=fs.readFileSync(htmlfile);
 response.send(html);
